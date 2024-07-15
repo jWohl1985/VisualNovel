fb11f84028d923f4d2bfe92d47b0980a6e8dbc16
Jeremy Wohl <jdwohl@gmail.com>
2024-07-14 22:36:54 -0700

    Added journal

--- scripts/GameInput.cs
+++ scripts/GameInput.cs
@@ -6,19 +6,50 @@ public partial class GameInput : Node
     [Export] private Control Environment;
     [Export] private Control UI;
     [Export] private Control Inventory;
+    [Export] private Control Journal;
+
+    private bool isJournalOpen = false;
+    private bool isInventoryOpen = false;
 
     public override void _Process(double delta)
     {
-        if (Input.IsActionJustPressed("RightClick"))
+        if (Input.IsActionJustPressed("RightClick") && !isJournalOpen && !isInventoryOpen)
         {
             UI.Visible = !UI.Visible;
         }
 
         if (Input.IsActionJustPressed("Inventory"))
         {
-            Environment.Visible = !Environment.Visible;
-            UI.Visible = !UI.Visible;
-            Inventory.Visible = !Inventory.Visible;
+            if (!isInventoryOpen)
+            {
+                Environment.Visible = false;
+                Inventory.Visible = true;
+                isInventoryOpen = true;
+            }
+            else
+            {
+                Environment.Visible = true;
+                Inventory.Visible = false;
+                isInventoryOpen = false;
+            }
+
+        }
+
+        if (Input.IsActionJustPressed("Journal"))
+        {
+            if (!isJournalOpen)
+            {
+                Environment.Visible = true;
+                Inventory.Visible = false;
+                Journal.Visible = true;
+                isJournalOpen = true;
+                isInventoryOpen = false;
+            }
+            else
+            {
+                Journal.Visible = false;
+                isJournalOpen = false;
+            }
         }
     }
 }
